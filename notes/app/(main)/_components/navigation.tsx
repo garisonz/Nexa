067c97177a010d3dcd5f598ebd325a78335ff404
garisonz <garisonzag88@gmail.com>
2024-09-04 14:10:07 -0400

    Document List Functionality

--- notes/app/(main)/_components/navigation.tsx
+++ notes/app/(main)/_components/navigation.tsx
@@ -9,15 +9,15 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./useritem";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
 import Item from "./item";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const documents = useQuery(api.documents.get);
     const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
@@ -125,9 +125,7 @@ export const Navigation = () => {
                     <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
                 </div>
                 <div className="mt-4">
-                    {documents?.map((document) => (
-                        <p key={document._id}>{document.title}</p>
-                    ))}
+                    <DocumentList />
                 </div>
                 <div onMouseDown={handleMouseDown} onClick={resetWidth} className="opacity-0 group-hover/sidebar:opacity-100
                 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"/>
