6f4b9d7240cf039a1414df90fb64f4d7014625ac
garisonz <garisonzag88@gmail.com>
2025-07-10 19:33:01 -0400

    Banner and toolbar component complete

---
+++ notes/app/(main)/_components/icon-picker.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import EmojiPicker, { Theme } from "emoji-picker-react";
+
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger
+} from "@/components/ui/popover"
+
+interface IconPickerProps {
+    onChange: (icon: string) => void;
+    children: React.ReactNode;
+    asChild?: boolean;
+};
+
+export const IconPicker = ({
+    onChange,
+    children,
+    asChild
+}: IconPickerProps) => {
+
+
+    const theme = "light";
+
+    return (
+        <Popover>
+            <PopoverTrigger asChild={asChild}>
+                {children}
+            </PopoverTrigger>
+            <PopoverContent className="p-0 w-full border-none shadow-none">
+                <EmojiPicker 
+                    height={350}
+                    onEmojiClick={(data) => onChange(data.emoji)}
+
+                />
+            </PopoverContent>
+        </Popover>
+    )
+}
--- notes/convex/documents.ts
+++ notes/convex/documents.ts
@@ -264,7 +264,7 @@ export const update = mutation({
         title: v.optional(v.string()),
         content: v.optional(v.string()),
         coverImage: v.optional(v.string()),
-        Icon: v.optional(v.string()),
+        icon: v.optional(v.string()),
         isPublished: v.optional(v.boolean())
     },
     handler: async (ctx, args) => {
@@ -294,4 +294,33 @@ export const update = mutation({
 
         return document;
     },
+});
+
+export const removeIcon = mutation({
+    args: { id: v.id("documents") },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        const existingDocument = await ctx.db.get(args.id);
+
+        if (!existingDocument) {
+            throw new Error("Not found");
+        }
+
+        if (existingDocument.userId !== userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const document = await ctx.db.patch(args.id, {
+            icon: undefined
+        });
+
+        return document;
+    }
 });
