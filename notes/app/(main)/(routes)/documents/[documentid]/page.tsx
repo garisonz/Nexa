e5dc23c7e94eeec77255234c9c54ee31f867a40a
garisonz <garisonzag88@gmail.com>
2025-07-11 23:01:21 -0400

    cover image in progress

--- notes/app/(main)/(routes)/documents/[documentid]/page.tsx
+++ notes/app/(main)/(routes)/documents/[documentid]/page.tsx
@@ -30,8 +30,8 @@ const DocumentIdPage = ({
         return <div>Not found</div>
     }
     return (
-        <div className="pb-40">
-            <div className="h-[35vh]"/>
+        <div className="pb-20">
+            <div className="h-[10vh]"/>
             <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                 <Toolbar initialData={document} />
             </div>
